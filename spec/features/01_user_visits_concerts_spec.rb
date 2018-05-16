469c49e3188f2d781f0a628c0d609b366b3c4a45
Eric Schaeffer <eschaeffer108@gmail.com>
2018-05-16 11:10:03 -0400

    new-branch

--- spec/features/01_user_visits_concerts_spec.rb
+++ spec/features/01_user_visits_concerts_spec.rb
@@ -1,24 +1,24 @@
-require 'rails_helper'
-
-feature "visitor sees a list of concerts" do
-  scenario "sees a list of concerts and a link to add a new concert" do
-    the_who = Concert.create(artist: "The Who", show_date: Date.new(2018-07-21), show_time: "7:00", description: "Come see the greatest band in the world, with original members Roger Daltrey and Pete Townshend! Plus Pete's brother Simon, and Ringo Starr's son, Zak Starkey, on Drums!")
-    alice_cooper = Concert.create(artist: "Alice Cooper", show_date: Date.new(2018-9-22), show_time: "8:00", description: "Come see the man, the myth, the legend: Alice Cooper! The king of shock rock will be performing his classic hits and you know he's bound to die on stage like he always does!")
-
-    visit concerts_path
-    expect(page).to have_content "The Who"
-    expect(page).to have_link "Alice Cooper"
-  end
-
-  scenario "vistior clicks on a link and is brought to that concert's show page" do
-    the_who = Concert.create(artist: "The Who", show_date: Date.new(2018-07-21), show_time: "7:00", description: "Come see the greatest band in the world, with original members Roger Daltrey and Pete Townshend! Plus Pete's brother Simon, and Ringo Starr's son, Zak Starkey, on Drums!")
-    visit concerts_path
-
-    click_link "The Who"
-
-    expect(page).to have_content the_who.artist
-    expect(page).to have_content the_who.show_date
-    expect(page).to have_content the_who.show_time
-    expect(page).to have_content the_who.description
-  end
-end
+# require 'rails_helper'
+#
+# feature "visitor sees a list of concerts" do
+#   scenario "sees a list of concerts and a link to add a new concert" do
+#     the_who = Concert.create(artist: "The Who", show_date: Date.new(2018-07-21), show_time: "7:00", description: "Come see the greatest band in the world, with original members Roger Daltrey and Pete Townshend! Plus Pete's brother Simon, and Ringo Starr's son, Zak Starkey, on Drums!")
+#     alice_cooper = Concert.create(artist: "Alice Cooper", show_date: Date.new(2018-9-22), show_time: "8:00", description: "Come see the man, the myth, the legend: Alice Cooper! The king of shock rock will be performing his classic hits and you know he's bound to die on stage like he always does!")
+#
+#     visit concerts_path
+#     expect(page).to have_content "The Who"
+#     expect(page).to have_link "Alice Cooper"
+#   end
+#
+#   scenario "vistior clicks on a link and is brought to that concert's show page" do
+#     the_who = Concert.create(artist: "The Who", show_date: Date.new(2018-07-21), show_time: "7:00", description: "Come see the greatest band in the world, with original members Roger Daltrey and Pete Townshend! Plus Pete's brother Simon, and Ringo Starr's son, Zak Starkey, on Drums!")
+#     visit concerts_path
+#
+#     click_link "The Who"
+#
+#     expect(page).to have_content the_who.artist
+#     expect(page).to have_content the_who.show_date
+#     expect(page).to have_content the_who.show_time
+#     expect(page).to have_content the_who.description
+#   end
+# end
--- spec/features/02_user_adds_new_concerts_spec.rb
+++ spec/features/02_user_adds_new_concerts_spec.rb
@@ -1,27 +1,27 @@
-require 'rails_helper'
-
-feature "visitor adds a new concert" do
-  scenario "visitor adds a new concert successfully" do
-    the_who = Concert.create(artist: "The Who", show_date: Date.new(2018-07-21), show_time: "7:00", description: "Come see the greatest band in the world, with original members Roger Daltrey and Pete Townshend! Plus Pete's brother Simon, and Ringo Starr's son, Zak Starkey, on Drums!")
-
-    visit new_concert_path
-
-    expect(page).to have_content "Add A New Concert"
-
-    fill_in 'Artist', with: "REO Speedwagon"
-    fill_in 'Description', with: "You've gotta roll with the changes!"
-    click_button 'Add Concert'
-  end
-
-  scenario "visitor does not provide proper information for the concert" do
-    the_who = Concert.create(artist: "The Who", show_date: Date.new(2018-07-21), show_time: "7:00", description: "Come see the greatest band in the world, with original members Roger Daltrey and Pete Townshend! Plus Pete's brother Simon, and Ringo Starr's son, Zak Starkey, on Drums!")
-
-    visit new_concert_path
-    click_button 'Add Concert'
-    save_and_open_page
-    expect(page).to have_content "Artist can't be blank"
-    expect(page).to have_content "Show date can't be blank"
-    expect(page).to have_content "Show time can't be blank"
-    expect(page).to have_content "Description can't be blank"
-  end
-end
+# require 'rails_helper'
+#
+# feature "visitor adds a new concert" do
+#   scenario "visitor adds a new concert successfully" do
+#     the_who = Concert.create(artist: "The Who", show_date: Date.new(2018-07-21), show_time: "7:00", description: "Come see the greatest band in the world, with original members Roger Daltrey and Pete Townshend! Plus Pete's brother Simon, and Ringo Starr's son, Zak Starkey, on Drums!")
+#
+#     visit new_concert_path
+#
+#     expect(page).to have_content "Add A New Concert"
+#
+#     fill_in 'Artist', with: "REO Speedwagon"
+#     fill_in 'Description', with: "You've gotta roll with the changes!"
+#     click_button 'Add Concert'
+#   end
+#
+#   scenario "visitor does not provide proper information for the concert" do
+#     the_who = Concert.create(artist: "The Who", show_date: Date.new(2018-07-21), show_time: "7:00", description: "Come see the greatest band in the world, with original members Roger Daltrey and Pete Townshend! Plus Pete's brother Simon, and Ringo Starr's son, Zak Starkey, on Drums!")
+#
+#     visit new_concert_path
+#     click_button 'Add Concert'
+#     save_and_open_page
+#     expect(page).to have_content "Artist can't be blank"
+#     expect(page).to have_content "Show date can't be blank"
+#     expect(page).to have_content "Show time can't be blank"
+#     expect(page).to have_content "Description can't be blank"
+#   end
+# end
